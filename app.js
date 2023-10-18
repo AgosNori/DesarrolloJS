ee868b33fd2a8bfc473b99e0f5d7de5ed861442c
agos <agosnoriega2018@gmail.com>
2023-10-18 17:41:35 -0300

    agrengadno nuevas funciones para el proyecto

--- app.js
+++ app.js
@@ -63,3 +63,80 @@ let count = 0;
 let chosenWord = "";
 
 
+/* Trabajamos con el display de las opciones*/
+const displayOptions = () => {
+    // alt+96 hace el template
+    optionsContainer.innerHTML += `<h3> Porfavor seleccione una opción</h3>`;
+    // innerHTML nos sirve para poder ingresar codigo html en js
+    let buttonCon = document.createElement("div");
+    for (let value in options) {
+        buttonCon.innerHTML += `<button class="options" onClick="generateWord('${value}')">${value}</button> `;
+    }
+    // appendChild les va a agregar al ultimo el boton
+    optionsContainer.appendChild(buttonCon);
+};
+
+const blocker = () => {
+    let optionsButton = document.querySelectorAll('.options');
+    let letterButton = document.querySelectorAll('.letters');
+
+    optionsButton.forEach((button) => {
+        button.disabled = true;
+    });
+
+    letterButton.forEach((button) => {
+        button.disabled = true;
+    });
+    newGameContainer.classList.remove("hide");
+};
+// generador de las palabras
+const generateWord = (optionValue) => {
+    let optionsButton = document.querySelectorAll(".options");
+    optionsButton.forEach((button) => {
+        if (button.innerText.toLowerCase() = optionValue) {
+            button.classList.add("active");
+        }
+        button.disabled = true;
+    });
+    // inicializamos el contenido de las letras en cero y limpiamos lo anterior
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText="";
+
+    let optionArray = options[optionValue];
+
+    // elegir una palabra aleatoria
+    chosenWord = optionArray[Math.floor(Math.ramdom()* optionArray.length)];
+    chosenWord = chosenWord.toUpperCase();
+
+    // una vez que ya selecciono la palabra
+    // por cada letra vamos a reemplazarlo por un signo
+
+    let displayItem = chosenWord.replace(/./g,'<span class="dashes">_</span>');
+    userInputSection.innerHTML=displayItem;
+};
+// cuando presionemos el boton de nuevo juego, se debe reinciar todo
+const initializer=()=>{
+    winCount=0;
+    count=0;
+
+    userInputSection.innerHTML="";
+    optionsContainer.innerHTML="";
+    letterContainer.classList.add("hide");
+    newGameContainer.classList.add("hide");
+    letterContainer.innerHTML="";
+
+
+    // crear las letras
+    for (let i= 65 ; i<91; i++){
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText= String.fromCharCode(i);
+
+        button.addEventListener("click",()=>{
+            // split()divide un objeto en string ( cadenas de texto)
+            let charArray=chosenWord.split("");
+            let dashes= document.getElementsByClassName("dashes");
+        })
+    }
+}
+
